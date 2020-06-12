77ebf9e80d2b86ff80a6b7071470ab8815289bdb
dllesso <49500732+dllesso@users.noreply.github.com>
2020-06-13 01:24:28 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <meta http-equiv='refresh' content='3; url=https://dllesso.github.io/'>
 <meta charset="UTF-8" />
 <?php
-$post=$_POST['banket1'];
+
 if (isset($_POST['name'])) {$name = $_POST['name']; if ($name == '') {unset($name);}}
 if (isset($_POST['mail'])) {$mail = $_POST['mail']; if ($mail == '') {unset($mail);}}
 if (isset($_POST['body'])) {$body = $_POST['body']; if ($body == '') {unset($body);}}
@@ -9,12 +9,12 @@ if (isset($_POST['body'])) {$body = $_POST['body']; if ($body == '') {unset($bod
 if (isset($name) && isset($mail)&& isset($body)){
  
 $address = "dimfornys@rambler.ru";
-$addresss = "yuureey@gmail.com";
 
 
-$mes = "Имя: $name \email: $mail \nТекст: $body \nЗаказ: $post";
+
+$mes = "Имя: $name \email: $mail \nТекст: $body";
 $send = mail ($address,$name,$mes);
-$send1 = mail ($addresss,$name,$mes);
+
 
 if ($send == 'true')
 {echo "Заказ сделан, через 3 секунды Вы будете направлены на главную страницу, где сможете продолжить просмотр.";
