ea1187eccdb4a0172dd172508705f12cbd7c8fb6
Dmitry Shcherbakov <49500732+dllesso@users.noreply.github.com>
2020-07-23 18:26:53 +0300

    Add files via upload

---
+++ rack/script.js
@@ -0,0 +1,249 @@
+   a.onmousedown = function funk(event) {
+
+  let shiftX = event.clientX - a.getBoundingClientRect().left;
+  let shiftY = event.clientY - a.getBoundingClientRect().top;
+
+  a.style.position = 'absolute';
+  a.style.zIndex = 1000;
+  document.body.append(a);
+  moveAt(event.pageX, event.pageY);
+  function moveAt(pageX, pageY) {
+  a.style.left = pageX - shiftX + 'px';
+  a.style.top = pageY - shiftY + 'px';
+  }
+
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+  document.addEventListener('mousemove', onMouseMove);
+  a.onmouseup = function funkkk() {
+    document.removeEventListener('mousemove', onMouseMove);
+   a.onmouseup = null;
+  };
+
+};a.ondragstart = function funkk() {
+  return false;
+};
+/*******************************************************/
+
+a1.onmousedown = function funk1(event) {
+  let shiftX = event.clientX - a1.getBoundingClientRect().left;
+  let shiftY = event.clientY - a1.getBoundingClientRect().top;
+ a1.style.position = 'absolute';
+  a1.style.zIndex = 1000;
+  document.body.append(a1);
+  moveAt(event.pageX, event.pageY);
+  function moveAt(pageX, pageY) {
+    a1.style.left = pageX - shiftX + 'px';
+    a1.style.top = pageY - shiftY + 'px';
+  }
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+  document.addEventListener('mousemove', onMouseMove);
+ a1.onmouseup = function funkkk1() {
+    document.removeEventListener('mousemove', onMouseMove);
+    a1.onmouseup = null;
+  };
+
+};a1.ondragstart = function funkk1() {
+  return false;
+};
+/*******************************************************/
+a2.onmousedown = function funk1(event) {
+  let shiftX = event.clientX - a2.getBoundingClientRect().left;
+  let shiftY = event.clientY - a2.getBoundingClientRect().top;
+ a2.style.position = 'absolute';
+  a2.style.zIndex = 1000;
+  document.body.append(a2);
+  moveAt(event.pageX, event.pageY);
+  function moveAt(pageX, pageY) {
+    a2.style.left = pageX - shiftX + 'px';
+    a2.style.top = pageY - shiftY + 'px';
+  }
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+  document.addEventListener('mousemove', onMouseMove);
+ a2.onmouseup = function funkkk1() {
+    document.removeEventListener('mousemove', onMouseMove);
+    a2.onmouseup = null;
+  };
+
+};a2.ondragstart = function funkk1() {
+  return false;
+};
+/*******************************************************/
+a3.onmousedown = function funk1(event) {
+  let shiftX = event.clientX - a3.getBoundingClientRect().left;
+  let shiftY = event.clientY - a3.getBoundingClientRect().top;
+ a3.style.position = 'absolute';
+  a3.style.zIndex = 1000;
+  document.body.append(a3);
+  moveAt(event.pageX, event.pageY);
+  function moveAt(pageX, pageY) {
+    a3.style.left = pageX - shiftX + 'px';
+    a3.style.top = pageY - shiftY + 'px';
+  }
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+  document.addEventListener('mousemove', onMouseMove);
+ a3.onmouseup = function funkkk1() {
+    document.removeEventListener('mousemove', onMouseMove);
+    a3.onmouseup = null;
+  };
+
+};a3.ondragstart = function funkk1() {
+  return false;
+};
+/*******************************************************/
+a4.onmousedown = function funk1(event) {
+  let shiftX = event.clientX - a4.getBoundingClientRect().left;
+  let shiftY = event.clientY - a4.getBoundingClientRect().top;
+ a4.style.position = 'absolute';
+  a4.style.zIndex = 1000;
+  document.body.append(a4);
+  moveAt(event.pageX, event.pageY);
+  function moveAt(pageX, pageY) {
+    a4.style.left = pageX - shiftX + 'px';
+    a4.style.top = pageY - shiftY + 'px';
+  }
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+  document.addEventListener('mousemove', onMouseMove);
+ a4.onmouseup = function funkkk1() {
+    document.removeEventListener('mousemove', onMouseMove);
+    a4.onmouseup = null;
+  };
+
+};a4.ondragstart = function funkk1() {
+  return false;
+};
+/*******************************************************/
+a5.onmousedown = function funk1(event) {
+  let shiftX = event.clientX - a5.getBoundingClientRect().left;
+  let shiftY = event.clientY - a5.getBoundingClientRect().top;
+ a5.style.position = 'absolute';
+  a5.style.zIndex = 1000;
+  document.body.append(a5);
+  moveAt(event.pageX, event.pageY);
+  function moveAt(pageX, pageY) {
+    a5.style.left = pageX - shiftX + 'px';
+    a5.style.top = pageY - shiftY + 'px';
+  }
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+  document.addEventListener('mousemove', onMouseMove);
+ a5.onmouseup = function funkkk1() {
+    document.removeEventListener('mousemove', onMouseMove);
+    a5.onmouseup = null;
+  };
+
+};a5.ondragstart = function funkk1() {
+  return false;
+};
+/*******************************************************/
+
+a6.onmousedown = function funk1(event) {
+  let shiftX = event.clientX - a6.getBoundingClientRect().left;
+  let shiftY = event.clientY - a6.getBoundingClientRect().top;
+ a6.style.position = 'absolute';
+  a6.style.zIndex = 1000;
+  document.body.append(a6);
+  moveAt(event.pageX, event.pageY);
+  function moveAt(pageX, pageY) {
+    a6.style.left = pageX - shiftX + 'px';
+    a6.style.top = pageY - shiftY + 'px';
+  }
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+  document.addEventListener('mousemove', onMouseMove);
+ a6.onmouseup = function funkkk1() {
+    document.removeEventListener('mousemove', onMouseMove);
+    a6.onmouseup = null;
+  };
+
+};a6.ondragstart = function funkk1() {
+  return false;
+};
+/*******************************************************/
+
+a7.onmousedown = function funk1(event) {
+  let shiftX = event.clientX - a7.getBoundingClientRect().left;
+  let shiftY = event.clientY - a7.getBoundingClientRect().top;
+ a7.style.position = 'absolute';
+  a7.style.zIndex = 1000;
+  document.body.append(a7);
+  moveAt(event.pageX, event.pageY);
+  function moveAt(pageX, pageY) {
+    a7.style.left = pageX - shiftX + 'px';
+    a7.style.top = pageY - shiftY + 'px';
+  }
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+  document.addEventListener('mousemove', onMouseMove);
+ a7.onmouseup = function funkkk1() {
+    document.removeEventListener('mousemove', onMouseMove);
+    a7.onmouseup = null;
+  };
+
+};a7.ondragstart = function funkk1() {
+  return false;
+};
+/*******************************************************/
+
+a8.onmousedown = function funk1(event) {
+  let shiftX = event.clientX - a8.getBoundingClientRect().left;
+  let shiftY = event.clientY - a8.getBoundingClientRect().top;
+ a8.style.position = 'absolute';
+  a8.style.zIndex = 1000;
+  document.body.append(a8);
+  moveAt(event.pageX, event.pageY);
+  function moveAt(pageX, pageY) {
+    a8.style.left = pageX - shiftX + 'px';
+    a8.style.top = pageY - shiftY + 'px';
+  }
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+  document.addEventListener('mousemove', onMouseMove);
+ a8.onmouseup = function funkkk1() {
+    document.removeEventListener('mousemove', onMouseMove);
+    a8.onmouseup = null;
+  };
+
+};a8.ondragstart = function funkk1() {
+  return false;
+};
+/*******************************************************/
+
+
+a9.onmousedown = function funk1(event) {
+  let shiftX = event.clientX - a9.getBoundingClientRect().left;
+  let shiftY = event.clientY - a9.getBoundingClientRect().top;
+ a9.style.position = 'absolute';
+  a9.style.zIndex = 1000;
+  document.body.append(a9);
+  moveAt(event.pageX, event.pageY);
+  function moveAt(pageX, pageY) {
+    a9.style.left = pageX - shiftX + 'px';
+    a9.style.top = pageY - shiftY + 'px';
+  }
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+  document.addEventListener('mousemove', onMouseMove);
+ a9.onmouseup = function funkkk1() {
+    document.removeEventListener('mousemove', onMouseMove);
+    a9.onmouseup = null;
+  };
+
+};a9.ondragstart = function funkk1() {
+  return false;
+};
+/*******************************************************/
